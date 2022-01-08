c1e1ceb0cecc203b6aa9b550d6d3bbe16ae24838
Alexandra <hajdu.alexandra09@gmail.com>
2022-01-08 15:26:36 +0100

    Initial commit

---
+++ src/test/java/com/hlxndr/jobsearchapplication/JobsearchapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hlxndr.jobsearchapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobsearchapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
