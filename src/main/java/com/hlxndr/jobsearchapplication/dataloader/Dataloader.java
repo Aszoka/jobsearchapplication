6059fead802566567ee81cbaac67a437811685af
Alexandra <hajdu.alexandra09@gmail.com>
2022-01-11 14:06:20 +0100

    refactor

--- src/main/java/com/hlxndr/jobsearchapplication/dataloader/Dataloader.java
+++ src/main/java/com/hlxndr/jobsearchapplication/dataloader/Dataloader.java
@@ -17,15 +17,18 @@ public class Dataloader {
     @Bean
     public CommandLineRunner run(ClientRepo repo, PositionRepo posRepo) {
         return args -> {
-            repo.save(new ClientApp("james@gmail.com","James", UUID.randomUUID()));
-            repo.save(new ClientApp("jane@gmail.com","Jane", UUID.randomUUID()));
+            ClientApp client1 = new ClientApp("testcompany1@gmail.com","testname1", UUID.randomUUID());
+            ClientApp client2 = new ClientApp("testcompany2@gmail.com","testname2", UUID.randomUUID());
+            repo.save(client1);
+            repo.save(client2);
+
             List<ClientApp> allClients = repo.findAll();
             System.out.println("All clients in DB: " + allClients);
 
-            posRepo.save(new Position("junior java developer", "Budapest"));
-            posRepo.save(new Position("data analyst", "Vienna"));
-            posRepo.save(new Position("graphic designer", "Budapest"));
-            posRepo.save(new Position("senior java developer", "Budapest"));
+            posRepo.save(new Position("junior java developer", "Budapest", client1));
+            posRepo.save(new Position("data analyst", "Vienna", client2));
+            posRepo.save(new Position("graphic designer", "Budapest", client1));
+            posRepo.save(new Position("senior java developer", "Budapest", client2));
             List<Position> poses = posRepo.findAll();
             System.out.println("poses right now: " + poses);
 
