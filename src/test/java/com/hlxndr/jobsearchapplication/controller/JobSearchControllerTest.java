25cf0e95121dc9a4e9fd0b543286819305221d93
Alexandra <hajdu.alexandra09@gmail.com>
2022-01-11 14:52:21 +0100

    Test classes added for future testing

---
+++ src/test/java/com/hlxndr/jobsearchapplication/controller/JobSearchControllerTest.java
@@ -0,0 +1,25 @@
+package com.hlxndr.jobsearchapplication.controller;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration
+@AutoConfigureMockMvc
+class JobSearchControllerTest {
+
+    @Test
+    void registerClient() {
+    }
+
+    @Test
+    void addPosition() {
+    }
+
+    @Test
+    void listSearchedPositions() {
+    }
+}
