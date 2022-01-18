b800d5c1861db01be7ad901636f701afd87178d8
Alexandra <hajdu.alexandra09@gmail.com>
2022-01-18 12:41:40 +0100

    refactor

--- src/main/java/com/hlxndr/jobsearchapplication/repository/ClientRepo.java
+++ src/main/java/com/hlxndr/jobsearchapplication/repository/ClientRepo.java
@@ -4,8 +4,10 @@ import com.hlxndr.jobsearchapplication.model.ClientApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ClientRepo extends JpaRepository<ClientApp, String> {
 
     Optional<ClientApp> findByEmail(String email);
+    Optional<ClientApp> findByOwnApiKey(UUID apiKey);
 }
